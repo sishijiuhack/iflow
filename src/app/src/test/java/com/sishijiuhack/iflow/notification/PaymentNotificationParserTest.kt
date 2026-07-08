42097bb420ec60591fc8cabbf35878517a8d8887
sishijiuhack <1523915641@qq.com>
2026-07-09 02:33:11 +0800

    Parse bank merchant name labels

--- src/app/src/test/java/com/sishijiuhack/iflow/notification/PaymentNotificationParserTest.kt
+++ src/app/src/test/java/com/sishijiuhack/iflow/notification/PaymentNotificationParserTest.kt
@@ -114,6 +114,23 @@ class PaymentNotificationParserTest {
     }
 
     @Test
+    fun parse_bankMerchantNameLabel_extractsMerchant() {
+        val result = parser.parse(
+            PaymentNotificationInput(
+                packageName = "com.example.bank",
+                title = "交易提醒",
+                text = "尾号1234支出人民币16.20元，商户名称：地铁",
+                postedAt = 100_000L,
+            ),
+        )
+
+        assertNotNull(result)
+        assertEquals(TransactionType.Expense, result?.type)
+        assertEquals(1620L, result?.amountCents)
+        assertEquals("地铁", result?.merchant)
+    }
+
+    @Test
     fun parse_unionPayExpense_extractsExpenseMerchantAndSource() {
         val result = parser.parse(
             PaymentNotificationInput(
