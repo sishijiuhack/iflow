d7315aaf70bd7f7c41aead08153c10148fc4a884
sishijiuhack <1523915641@qq.com>
2026-07-09 03:43:58 +0800

    Parse currency prefixes case-insensitively

--- src/app/src/test/java/com/sishijiuhack/iflow/notification/PaymentNotificationParserTest.kt
+++ src/app/src/test/java/com/sishijiuhack/iflow/notification/PaymentNotificationParserTest.kt
@@ -430,6 +430,23 @@ class PaymentNotificationParserTest {
     }
 
     @Test
+    fun parse_amountWithLowercaseCurrencyPrefix_extractsAmount() {
+        val result = parser.parse(
+            PaymentNotificationInput(
+                packageName = "com.example.bank",
+                title = "交易提醒",
+                text = "消费rmb16.20，商户：地铁",
+                postedAt = 100_000L,
+            ),
+        )
+
+        assertNotNull(result)
+        assertEquals(TransactionType.Expense, result?.type)
+        assertEquals(1620L, result?.amountCents)
+        assertEquals("地铁", result?.merchant)
+    }
+
+    @Test
     fun parse_irrelevantNotification_returnsNull() {
         val result = parser.parse(
             PaymentNotificationInput(
