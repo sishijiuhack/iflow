b91993f3978566c900607004d0c519d7e1b64765
sishijiuhack <1523915641@qq.com>
2026-07-09 01:46:06 +0800

    Cover time formatter edges

--- src/app/src/test/java/com/sishijiuhack/iflow/core/model/FormattersTest.kt
+++ src/app/src/test/java/com/sishijiuhack/iflow/core/model/FormattersTest.kt
@@ -18,6 +18,14 @@ class FormattersTest {
     }
 
     @Test
+    fun editableTime_trimsUserInput() {
+        val millis = parseEditableTime(" 2026-07-08 23:15 ", zone)
+
+        assertNotNull(millis)
+        assertEquals("2026-07-08 23:15", millis!!.formatEditableTime(zone))
+    }
+
+    @Test
     fun editableTime_rejectsInvalidInput() {
         assertNull(parseEditableTime("2026/07/08 23:15", zone))
         assertNull(parseEditableTime("not time", zone))
@@ -29,5 +37,13 @@ class FormattersTest {
 
         assertNotNull(millis)
         assertEquals("20260708-231500", millis!!.formatExportFileTime(zone))
+    }
+
+    @Test
+    fun ledgerTime_usesMonthDayAndMinute() {
+        val millis = parseEditableTime("2026-07-08 23:15", zone)
+
+        assertNotNull(millis)
+        assertEquals("07-08 23:15", millis!!.formatLedgerTime(zone))
     }
 }
